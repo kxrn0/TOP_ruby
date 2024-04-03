46007e228c0011e87436a185e0c4c136903b992e
kxrn0 <kxrp@protonmail.com>
2024-04-03 11:38:11 -0500

    commiting

---
+++ 02_basic_ruby/03_input_and_output/rybu.rb
@@ -0,0 +1,31 @@
+=begin
+  1.- What are the two methods we can use to output data to the user's screen?
+  
+  We can use puts and print to print data to the console.
+=end
+
+=begin
+  2.- What is similar between these two output methods?
+
+  They both output a representation of the data we feed them to the console, and they both return nil.
+=end
+
+=begin
+  3.- What is different between these two output methods?
+
+  puts tries to convert everything to a string, so nil will be converted to "", while print tries to print a version of the
+  object that looks more like itself, for example nil is printed as nil. puts adds a newline character after printing, print
+  doesn't.
+=end
+
+=begin
+  4.- What is the method you can use to get input from the user?
+
+  gets
+=end
+
+=begin
+  5.- What is at least one difference between the input and output methods?
+
+  Input methods lets the user input data, while output methods let the user output data.
+=end
