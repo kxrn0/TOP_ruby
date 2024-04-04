acecd9d95ed2c35ba1a36d29fdd5b70dcb7d7d69
kxrn0 <kxrp@protonmail.com>
2024-04-04 12:26:28 -0500

    worked on loops

---
+++ 02_basic_ruby/05_loops/rybu.rb
@@ -0,0 +1,49 @@
+=begin
+  1.- What is a loop, and why is it useful?
+  
+  A loop is a piece of code that continues to be executed while some condition is true. It's useful for avoiding having to
+  manually write the same thing over and over.
+=end
+
+=begin
+  2.- What is a loop loop, and how would you use it?
+
+  A loop loop is a loop that loops while it's not broken. It starts with the loop keyword, and it's delimited with do/end. It
+  can be stopped with the break statement.
+=end
+
+=begin
+  3.- What is a while loop, and how would you use it?
+
+  A while loop is created with the while keyword. It continues to run while the expression next to the while keyword continues
+  to evaluate to true. It's delimited by do(optional)/end.
+=end
+
+=begin
+  4.- What is a for loop, and how would you use it?
+
+  For loops are loops where we iterate through the iterables of an iterable. We begin with the for keyword, followed by the
+  name of the variable that will hold the value of the item in the iterable currently being iterated, then the in keyword, and finally
+  the iterable, which can be an array, range, etc. The body is delimited by do/end.
+=end
+
+=begin
+  5.- What is a #times loop, and how would you use it?
+
+  A times loop is a method of a number n that makes the code in the body of the times run from zero up to n - 1. We have access to the
+  current value through the variable declared between pipes after the do that antecedes the call to the method. The body of is delimited
+  by do/end.
+=end
+
+=begin
+  6.- What is an until loop, and how would you use it?
+
+  It's the opposite of while. It keeps running until its condition is true.
+=end
+
+=begin
+  7.- What are #upto and #downto loops, and how would you use them?
+
+  They are number methods which take a number as input. They both start at the current value. #upto keeps running until the current value is equal 
+  to the provided value. #downto is similar, but the current value decreases from the current value down to the target value.
+=end
