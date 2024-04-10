ad9eac3bf73477bdb9a96f07ea76e4657d5994fe
kxrn0 <kxrp@protonmail.com>
2024-04-10 11:30:16 -0500

    finished the predicate enumerables section

---
+++ 02_basic_ruby/11_predicate_enumerable_methods/rybu.rb
@@ -0,0 +1,29 @@
+=begin
+  1.- Why is there a question mark after some method names?
+  
+  It indicates that the method returns a boolean value.
+=end
+
+=begin
+  2.- What does the #include? method do?
+
+  It returns true if the enumerable it's called on has the parameter it's provided, otherwise it returns false.
+=end
+
+=begin
+  3.- What does the #any? method do?
+
+  It returns true if there's at least one element in the enumerable it's called on for which the provided block returns true.
+=end
+
+=begin
+  4.- What does the #all? method do?
+
+  It returns true if every single element in the enumerable this method is called on makes the provided block return true.
+=end
+
+=begin
+  5.- What does the #none? method do?
+
+  It returns true if no element in the enumerable makes the block return true.
+=end
