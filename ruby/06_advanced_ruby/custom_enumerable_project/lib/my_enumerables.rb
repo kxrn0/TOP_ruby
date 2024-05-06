91e6d6c8a647ea03c8ab4727eec1bf7dce9b33e0
kxrn0 <kxrp@protonmail.com>
2024-05-06 09:01:36 -0500

    finished the enumerables

--- ruby/06_advanced_ruby/custom_enumerable_project/lib/my_enumerables.rb
+++ ruby/06_advanced_ruby/custom_enumerable_project/lib/my_enumerables.rb
@@ -1,5 +1,90 @@
 module Enumerable
   # Your code goes here
+
+  def my_each
+    for element in self
+      yield element
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    while i < self.size
+      yield self[i], i
+
+      i += 1
+    end
+
+    self
+  end
+
+  def my_select
+    pass = []
+
+    self.each do |item|
+      pass.push item if yield item
+    end
+
+    pass
+  end
+
+  def my_all?
+    self.each do |item|
+      value = yield item
+
+      return false unless value
+    end
+
+    true
+  end
+
+  def my_any?
+    self.each do |item|
+      return true if yield(item)
+    end
+    
+    false
+  end
+
+  def my_none?
+    self.each do |item|
+      value = yield item
+
+      return false if value
+    end
+
+    true
+  end
+
+  def my_count
+    return self.size unless block_given?
+
+    count = 0
+
+    self.reduce(0) do |tot, curr|
+      tot + (yield(curr) ? 1 : 0)
+    end
+  end
+
+  def my_map
+    map = []
+
+    self.each do |item|
+      map.push(yield item)
+    end
+
+    map
+  end
+
+  def my_inject(initial_value = 0)
+    total = initial_value
+
+    self.each do |item|
+      total = yield total, item
+    end
+
+    total
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +92,5 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  include Enumerable
 end
