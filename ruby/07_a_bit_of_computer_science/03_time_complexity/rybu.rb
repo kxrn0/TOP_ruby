bbdb3b1747e574a08610e1f58c2ed37a408fcca5
kxrn0 <kxrp@protonmail.com>
2024-05-15 12:59:35 -0500

    carry on

---
+++ ruby/07_a_bit_of_computer_science/03_time_complexity/rybu.rb
@@ -0,0 +1,25 @@
+=begin
+  1.- What is Big O?
+
+  A way to analyze how much time or resources an algorithm will take when running.
+=end
+
+=begin
+  2.- What are the Big O notations?
+
+  Big O: It gives the wost case scenario.
+  Big Omega: It gives the best case scenario.
+  Big Theta: It gives the average.
+=end
+
+=begin
+  3.- Why use Big O?
+
+  To get a rough estimate of how much input the system can cope with.
+=end
+
+=begin
+  4.- What is Big Omega, and why isn't as useful?
+
+  It gives the best case scenario for when running an algorithm. It's not as useful because it assumes an ideal case, which is not as common in the real world.
+=end
