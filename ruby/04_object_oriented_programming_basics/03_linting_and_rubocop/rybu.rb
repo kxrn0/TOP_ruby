259bdb4439a9cd0f2b58b43d058c8823f4e02bbf
kxrn0 <kxrp@protonmail.com>
2024-05-28 11:57:10 -0500

    rubbed

---
+++ ruby/04_object_oriented_programming_basics/03_linting_and_rubocop/rybu.rb
@@ -0,0 +1,76 @@
+=begin
+  1.- What is a style guide?
+
+  A guide for how code should be structured.
+=end
+
+=begin
+  2.- What is formatting?
+
+  Arranging the pieces of a program (variables, keywords, etc.) in a way that follows a convention. 
+=end
+
+=begin
+  3.- What is linting?
+
+  Analyzing the code without running it to determine potential bugs.
+=end
+
+=begin
+  4.- How do you install rubocop?
+
+  By adding it with bundle.
+=end
+
+=begin
+  5.- How do you run rubocop in a command line?
+
+  By executing it with bundle.
+=end
+
+=begin
+  6.- How do you read robocop's output?
+
+  The first line says how many files have been inspected.
+
+  The offenses display the line:column: Offense Type, cop, and description.
+=end
+
+=begin
+  7.- How do you configure rubocop?
+
+  With the .rubocop.yml file.
+=end
+
+=begin
+  8.- Why is metrics department so important for new rubysts?
+
+  It helps writting more performant code.
+=end
+
+=begin
+  9.- What is the ABC metric?
+
+  A code quality metric based on the number of Assignments, Branches (function calls), and Conditional expressions.
+=end
+
+=begin
+  10.- What is cyclomatic complexity?
+
+  A metric based on all possible paths the code can take.
+=end
+
+=begin
+  11.- What is perceived complexity?
+
+  A metric that attempts to measure how hard it would be for someone to understand the code.
+  Similar to cyclomatic complexity, it is based on the number of paths the code can take, but it also
+  takes into account else statements.
+=end
+
+=begin
+  12. How do you configure rubocop in vscode?
+
+  With a .rubocop.yml file.
+=end
+
