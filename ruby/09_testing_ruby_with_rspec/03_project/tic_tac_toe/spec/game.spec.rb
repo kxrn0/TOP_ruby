c4aa7fb0a9ce4ddb123bec9a0554afad8343a034
kxrn0 <kxrp@protonmail.com>
2024-08-21 21:01:01 -0500

    wake up

--- ruby/09_testing_ruby_with_rspec/03_project/tic_tac_toe/spec/game.spec.rb
+++ ruby/09_testing_ruby_with_rspec/03_project/tic_tac_toe/spec/game.spec.rb
@@ -1,7 +1,17 @@
 require_relative '../lib/game'
 
 describe Game do
-  # 
-  #
-  #
+  describe '#get_marker' do
+    context 'when called' do
+      before do
+        #...
+      end
+
+      it 'works when there\'s no previous marker' do
+        #...
+      end
+    end
+  end  
+  
+
 end
